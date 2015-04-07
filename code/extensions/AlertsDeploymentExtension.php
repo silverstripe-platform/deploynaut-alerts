0b6b86123d26ecc3d022f6b7b0f3b1ac4035a1b1
Sean Harvey <sean@silverstripe.com>
2015-04-08 11:42:48 +1200

    Adding placeholder for alerts deployment

---
+++ code/extensions/AlertsDeploymentExtension.php
@@ -0,0 +1,17 @@
+<?php
+class AlertsDeploymentExtension extends Extension {
+
+	/**
+	 * After a successful deployment, configure the Pingdom alerts
+	 * configured from the alerts.yml in the site code repository.
+	 *
+	 * @param DNEnvironment $environment
+	 * @param string $sha
+	 * @param DeploynautLogFile $log
+	 * @param DNProject $project
+	 */
+	public function deployEnd($environment, $sha, $log, $project) {
+		// todo
+	}
+
+}
