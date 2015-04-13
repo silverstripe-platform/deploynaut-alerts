bad91031838d4ae8b61bd7442d2587b387425e5d
Sean Harvey <sean@silverstripe.com>
2015-04-13 14:50:17 +1200

    Force use of ss_env for configuring pingdom credentials

--- _config.php
+++ _config.php
@@ -6,3 +6,15 @@ if(!defined('DEPLOYNAUT_OPS_EMAIL')) {
 if(!defined('DEPLOYNAUT_OPS_EMAIL_FROM')) {
 	throw new RuntimeException('You must set DEPLOYNAUT_OPS_EMAIL_FROM in _ss_environment.php');
 }
+if(!defined('PINGDOM_USERNAME')) {
+	throw new RuntimeException('You must set PINGDOM_USERNAME in _ss_environment.php');
+}
+if(!defined('PINGDOM_PASSWORD')) {
+	throw new RuntimeException('You must set PINGDOM_PASSWORD in _ss_environment.php');
+}
+if(!defined('PINGDOM_API_KEY')) {
+	throw new RuntimeException('You must set PINGDOM_API_KEY in _ss_environment.php');
+}
+
+$api = new \Acquia\Pingdom\PingdomApi(PINGDOM_USERNAME, PINGDOM_PASSWORD, PINGDOM_API_KEY);
+Injector::inst()->registerService($api, 'PingdomService');
